b65c723b39101b74c49510daa6d8157b60770fa1
Titty <tittystrength@gmail.com>
2023-08-31 16:08:35 +0330

    final code

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -6,8 +6,8 @@ function Layout({ children }) {
   return (
     <div className="w-full min-h-screen flex flex-col items-center font-Quicksand overflow-hidden">
       <Header />
-      <main className="w-full flex-1">
-        <div className="max-w-6xl h-full mx-auto py-10 px-5">{children}</div>
+      <main className="w-full flex-1 py-5 px-5">
+        <div className="max-w-6xl w-full h-full mx-auto">{children}</div>
       </main>
     </div>
   );
